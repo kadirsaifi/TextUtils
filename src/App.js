73ec15673db3e957e7d1bd578dd6365cb63b9c88
Abdul Kadir <48976007+kadirsaifi@users.noreply.github.com>
2024-05-12 15:15:09 +0400

    replace router
    for uplod on gituhb

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import "./App.css";
-import About from "./component/About";
+/* import About from "./component/About"; */
 import Navbar from "./component/Navbar";
 import Textform from "./component/Textform";
 /* import Colorpickergfg from "./component/Colorpickergfg"; */
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+/* import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; */
 function App() {
   const [mode, setMode]=useState('light');
 
@@ -20,20 +20,20 @@ function App() {
   }
   return (
     <>
-    <Router>
+    {/* <Router> */}
       <Navbar title={"TextUtils"} about={"About-Us"} mode={mode} toglemode={toglemode}/>
       {/*   */}
       
       <div className="container my-3">
-        <Routes>
-        <Route path="/about" element={<About/>}>
+    {/*     <Routes> */}
+        {/* <Route path="/about" element={<About/>}>
 
-        </Route>
-           <Route path="/" element={<Textform heading="Enter-text-here!" button="Convert Upper Case" mode={mode} lowercase="Convert to Lower Case" clear="Clear" copy="Copy Text" />}>
-           </Route>
-        </Routes>
+        </Route> */}
+           <Textform heading="Enter-text-here!" button="Convert Upper Case" mode={mode} lowercase="Convert to Lower Case" clear="Clear" copy="Copy Text" />
+          {/*  </Route>
+        </Routes> */}
       </div>
-      </Router>
+     {/*  </Router> */}
     </>
   );
 }
