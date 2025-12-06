20f735316bfc6cf570f85097f42f305e04fa9ceb
kadirsaifi <kadirsaifi08@gmail.com>
2025-12-06 13:18:06 +0400

    update from

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
+import About from './component/About';
 import Navbar from './component/Navbar';
-import TextForm from './component/TextForm';
+// import TextForm from './component/TextForm';
 
 function App() {
   return (
     <div className="App">
      <Navbar title="TextUtils" menu="Home"/>
      <div className="container my-3">
-      <TextForm headding="Enter the text here"/>
+      {/* <TextForm headding="Enter the text here"/> */}
+      <About/>
      </div>
      
     </div>
--- src/component/TextForm.js
+++ src/component/TextForm.js
@@ -1,39 +1,58 @@
-import React from 'react'
-import { useState } from 'react'
+import React from "react";
+import { useState } from "react";
 
 export default function TextForm(props) {
+  const handelChnageEvent = (e) => {
+    setText(e.target.value);
+  };
+  const HandelUpperCase = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+  const HandelLowerCase = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
+  const handelClear=()=>{
+    let newText ='';
+    setText(newText);
+  };
+  const handelSentence=()=>{
 
-    const handelChnageEvent=(e)=>{
-        setText(e.target.value)
-    }
-    const HandelUpperCase=()=>{
-        let upperCase = text.toUpperCase();
-        setText(upperCase);
-    }
-    const HandelLowerCase=()=>{
-         let loweCase = text.toLowerCase();
-         setText(loweCase);
-    }
+  };
 
-    const [text, setText]= useState("Enter Text Here!")
+  const [text, setText] = useState("Enter Text Here!");
   return (
     <>
-    <div className='container'>
+      <div className="container">
         <h1>{props.headding}</h1>
         <div className="mb-3">
-            <textarea className="form-control" value={text} onChange={handelChnageEvent} id="myBox" rows="8"></textarea>
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handelChnageEvent}
+            id="myBox"
+            rows="8"
+          ></textarea>
         </div>
-        <button className='btn btn-primary' onClick={HandelUpperCase}>Click Upper Case</button>
-        <button className='btn btn-primary mx-3' onClick={HandelLowerCase}>Click Lower Case</button>
-    </div>
-    <div className="container my-4">
+        <button className="btn btn-primary" onClick={HandelUpperCase}>
+          Click Upper Case
+        </button>
+        <button className="btn btn-primary mx-3" onClick={HandelLowerCase}>
+          Click Lower Case
+        </button>
+        <button className="btn btn-primary" onClick={handelClear}>Clear</button>
+        <button className="btn btn-primary mx-3" onClick={handelSentence}>Sentence case</button>
+      </div>
+      <div className="container my-4">
         <h1>Your Text Summary</h1>
-        <p>{text.split(" ").length} words and {text.length} characters</p>
-
+        <p>
+          {text.split(" ").length} words and {text.length} characters
+        </p>
         <p>{0.008 * text.split(" ").length} Minutes</p>
         <h3>Preview</h3>
         <p>{text}</p>
-    </div>
+      </div>
     </>
-  )
+  );
 }
