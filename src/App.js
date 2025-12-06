1da26636cb5b8e079adddd36834dc7eaad38a8f2
kadirsaifi <kadirsaifi08@gmail.com>
2025-12-07 02:36:39 +0400

    remove aler component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import About from './component/About';
 import Navbar from './component/Navbar';
 import TextForm from './component/TextForm';
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
-import Alert from './component/Alert';
 
 function App() {
 
@@ -27,7 +26,6 @@ const toggleMode = () => {
     <div className="App">
      <Router>
       <Navbar title="TextUtils" menu="Home" mode={mode} toggleMode={toggleMode}/>
-      <Alert/>
       <Routes>
         <Route path='/' element={<TextForm headding="Enter the text here" mode={mode}/>}></Route>
         <Route path='/about' element={<About/>}></Route>
